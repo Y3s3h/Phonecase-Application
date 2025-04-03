ca5753c2f7cb19e49f415fa2283e3690b9302be4
Y3s3h <yy6922341@gmail.com>
2025-04-03 20:08:11 +0530

    email updated

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
       })
 
       await resend.emails.send({
-        from: 'yy6922341@gmail.com',
+        from: 'yashyadav@gmail.com',
         to: [event.data.object.customer_details.email],
         subject: 'Thanks for your order!',
         react: OrderReceivedEmail({
