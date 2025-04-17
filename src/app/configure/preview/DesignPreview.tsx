f25690ddbf5f95adaa9498120c8ce5e8f43b2f19
Y3s3h <yy6922341@gmail.com>
2025-04-17 17:30:13 +0530

    user dependency removed

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -54,17 +54,21 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
       })
     },
   })
-
   const handleCheckout = () => {
-    if (user) {
-      // create payment session
-      createPaymentSession({ configId: id })
-    } else {
-      // need to log in
-      localStorage.setItem('configurationId', id)
-      setIsLoginModalOpen(true)
-    }
-  }
+    createPaymentSession({ configId: id });
+  };
+  
+
+  // const handleCheckout = () => {
+  //   if (user || !user) {
+  //     // create payment session
+  //     createPaymentSession({ configId: id })
+  //   } else {
+  //     // need to log in
+  //     localStorage.setItem('configurationId', id)
+  //     setIsLoginModalOpen(true)
+  //   }
+  // }
 
   return (
     <>
